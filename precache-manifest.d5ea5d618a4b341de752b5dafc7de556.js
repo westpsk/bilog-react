27dcb9effbaca3d1e0271122d50a8f8573d3faed
westpsk <pansongkui@126.com>
2019-08-11 23:26:04 +0800

    Updates

---
+++ precache-manifest.d5ea5d618a4b341de752b5dafc7de556.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7389392d44f9231df63bd60d0d1b0f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "481a942d1014887efad5",
+    "url": "/static/js/7.a2824951.chunk.js"
+  },
+  {
+    "revision": "2329fb5b036a96660e88",
+    "url": "/static/js/Welcome.c7af8791.chunk.js"
+  },
+  {
+    "revision": "8280c1315dcd3a1c4629",
+    "url": "/static/js/home.2a3776a2.chunk.js"
+  },
+  {
+    "revision": "c85db956c74c555183f7",
+    "url": "/static/js/login.9931b9cf.chunk.js"
+  },
+  {
+    "revision": "2175aa5f09e0a92a3630",
+    "url": "/static/js/main.49d18d65.chunk.js"
+  },
+  {
+    "revision": "ae57254403495a0a08d1",
+    "url": "/static/js/notfound.6dcabcff.chunk.js"
+  },
+  {
+    "revision": "69fc1c3b6d85265c0a6e",
+    "url": "/static/js/runtime~main.093d87cf.js"
+  },
+  {
+    "revision": "ac7ef68b8a55bcb31391",
+    "url": "/static/js/sort.911580d5.chunk.js"
+  },
+  {
+    "revision": "738360e235cbeb702630d1185ea1a451",
+    "url": "/static/media/index.738360e2.less"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bilog-react/precache-manifest.6c79891ba0760fad6108034b02c0ab4f.js"
+  "/precache-manifest.d5ea5d618a4b341de752b5dafc7de556.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bilog-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
