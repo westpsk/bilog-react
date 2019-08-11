b31eccd67bf99ab1a0a94c267da226b71ee602ed
westpsk <pansongkui@126.com>
2019-08-12 02:03:07 +0800

    sort

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (<div>
     Home
-    <Link to="/welcome">welcome</Link>
+    <Link to="/sort">to sort</Link>
   </div>)
 }
 
--- src/reducers.js
+++ src/reducers.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 // 通用
 import Common from 'reducer/index.js';
+import Sort from 'pages/Sort/reducer.js';
 
 export default combineReducers({
   Common,
+  Sort
 });
